aa3801ee01b1ca08f87487880edc62ba2d2cf637
kseniaDubova <ksushadubova@icloud.com>
2025-10-20 17:57:05 +0400

    fonts

--- front/App.js
+++ front/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import React, { useCallback } from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { mainStyle } from "./style";
+import { FONT_MAP } from "./assets/fonts";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts(FONT_MAP);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <SafeAreaView>
+        <Text>Загрузка</Text>
+      </SafeAreaView>
+    );
+  } else
+    return (
+      <SafeAreaView onLayout={onLayoutRootView}>
+        <Text style={mainStyle.title}>Приложение</Text>
+      </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ front/style/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const mainStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  title: {
+    fontFamily: "extra-bold",
+  },
+});
