ffb9189756e2089ab1d3bba39994db7230623a46
Cat-sandwich <beni@ya.ru>
2025-10-26 17:39:16 +0400

    init

--- front/style/index.js
+++ front/style/index.js
@@ -1,12 +1,6 @@
 import { StyleSheet } from "react-native";
 
 export const mainStyle = StyleSheet.create({
-  main: {
-    flex: 1,
-  },
-  title: {
-    fontFamily: "extra-bold",
-  },
   h1: {
     fontFamily: "regular",
     fontSize: 24,
