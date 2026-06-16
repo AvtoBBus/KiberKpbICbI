d3ca643c945cc73d496cbc3de703ece18ed6ae49
AvtoBBus <mister.voker163@yandex.ru>
2026-06-16 15:03:00 +0400

    upd db util

--- server/app/utils/db.py
+++ server/app/utils/db.py
@@ -1,8 +1,18 @@
+import ssl
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import config
 
-engine = create_async_engine(config.settings.database_url)
+def create_db_engine():
+    ssl_context = ssl.create_default_context(cafile="/etc/ssl/cert.pem")
+    
+    return create_async_engine(
+        config.settings.database_url,
+        connect_args={"ssl": ssl_context},
+        echo=False
+    )
+
+engine = create_db_engine()
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
